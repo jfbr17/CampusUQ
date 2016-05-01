c2b48b2a875ee590c1dc545a4d0227c434741686
jfbr17 <jfbr2015@gmail.com>
2016-05-01 01:16:54 -0500

    Initial commit

---
+++ app/src/main/java/campusuq/com/uniquindio/android/electiva/campusuq/NoticiasActivity.java
@@ -0,0 +1,13 @@
+package campusuq.com.uniquindio.android.electiva.campusuq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoticiasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noticias);
+    }
+}
